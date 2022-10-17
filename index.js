39e3ce616819ddbde9c16266d2ebc2210b0daaf4
Samuel Gaddis <siddag14@live.com>
2022-10-17 13:03:11 -0400

    commit tree 5219de5634cb6c5b752641b6e24ca867de809a22

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
+const link = "https://anapioficeandfire.com/api/books"
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch(link)
+  .then((res) => res.json())
+  .then((data) => renderBooks(data))
+
 }
 
 function renderBooks(books) {
+
   const main = document.querySelector('main');
-  books.forEach(book => {
+    books.forEach(book => {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
@@ -15,3 +20,7 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+function potato() {
+  return 'french fries'
+}
